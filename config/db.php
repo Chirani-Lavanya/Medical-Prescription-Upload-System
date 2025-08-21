dfbf687ae62e091995ce822499a99cc2f10a23c2
Chirani Lavanya <126225500+Chirani-Lavanya@users.noreply.github.com>
2025-08-21 13:55:31 +0530

    Add files via upload

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "prescription_system";
+$conn = new mysqli($host, $user, $pass, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
